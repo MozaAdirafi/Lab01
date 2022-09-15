bfd63fdef2a5691b5a8c485a6b78282a97132609
Moza Adirafi <mozaadirafi26@gmail.com>
2022-09-15 08:11:27 +0700

    update

--- wishlist/models.py
+++ wishlist/models.py
@@ -5,3 +5,4 @@ class ItemWishlist(models.Model):
     item_name = models.CharField(max_length=50)
     item_price = models.IntegerField()
     description = models.TextField()
+    
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,12 +1,34 @@
 from django.shortcuts import render
 from wishlist.models import ItemWishlist
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 def show_wishlist(request):
-    return render(request, "wishlist.html",context)
-
-data_wishlist_item = ItemWishlist.objects.all()
-context = {
+    context = {
     'list_item': data_wishlist_item,
     'name': 'Moza Adirafi Satria Jaka'
 }
+
+    return render(request, "wishlist.html",context)
+    
+data_wishlist_item = ItemWishlist.objects.all()
+
+
+def show_xml(request):
+    data = ItemWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+
+def show_json(request):
+    data = ItemWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request,id):
+    data = ItemWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+   
+
+def show_xml_by_id(request,id):
+    data = ItemWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
